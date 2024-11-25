bf6a53c4ec6d4821a11ed9b4301f6f40a1e689b1
William Henderson <william-henderson@outlook.com>
2024-11-25 11:47:42 +0000

    support syntactic sugar for lists

--- core/src/ast.rs
+++ core/src/ast.rs
@@ -19,6 +19,19 @@ pub enum Atom {
     Float(f64),
 }
 
+impl Term {
+    // Support syntactic sugar for lists.
+    pub fn list(terms: Vec<Term>, tail: Option<Term>) -> Term {
+        let mut term = tail.unwrap_or(Term::Atom(Atom::String("[]".to_string())));
+
+        for t in terms.into_iter().rev() {
+            term = Term::Compound(".".to_string(), vec![t, term]);
+        }
+
+        term
+    }
+}
+
 impl ToCodeTerm for Term {
     fn to_code_term(&self, string_map: &mut StringMap) -> CodeTerm {
         match self {
--- core/src/tests/core.rs
+++ core/src/tests/core.rs
@@ -1,22 +1,19 @@
 use crate::WebPL;
 
 static APP_PROGRAM: &str = r#"
-app(nil, L2, L2).
-app(cons(H, T), L2, cons(H, L3)) :- app(T, L2, L3).
+app([], L2, L2).
+app([H|T], L2, [H|L3]) :- app(T, L2, L3).
 "#;
 
 #[test]
 fn app() {
-    let query = "app(cons(1, cons(2, nil)), cons(3, cons(4, nil)), L).";
+    let query = "app([1, 2], [3, 4], L).";
     let mut webpl = WebPL::new(APP_PROGRAM).unwrap();
     let mut solver = webpl.solve(query).unwrap();
 
     assert_eq!(
         solver.next(),
-        Some(vec![(
-            "L".into(),
-            "cons(1, cons(2, cons(3, cons(4, nil))))".into()
-        )])
+        Some(vec![("L".into(), ".(1, .(2, .(3, .(4, []))))".into())])
     );
 
     assert_eq!(solver.next(), None);
@@ -24,13 +21,13 @@ fn app() {
 
 #[test]
 fn recursive_solution() {
-    let query = "app(cons(1, cons(2, nil)), L, L).";
+    let query = "app([1, 2], L, L).";
     let mut webpl = WebPL::new(APP_PROGRAM).unwrap();
     let mut solver = webpl.solve(query).unwrap();
 
     assert_eq!(
         solver.next(),
-        Some(vec![("L".into(), "cons(1, cons(2, L))".into())])
+        Some(vec![("L".into(), ".(1, .(2, L))".into())])
     );
 
     assert_eq!(solver.next(), None);
@@ -73,20 +70,20 @@ fn multiple_goals() {
 #[ignore = "long in debug"]
 fn n_queens() {
     let program = r#"
-        take(cons(H, T), H, T).
-        take(cons(H, T), R, cons(H, S)) :- take(T, R, S).
-        perm(nil, nil).
-        perm(L, cons(H, R)) :- take(L, H, T), perm(T,R).
-        generate_list(1, cons(1, nil)).
-        generate_list(N, cons(N, T)) :- N > 1, M is N - 1, generate_list(M, T).
+        take([H|T], H, T).
+        take([H|T], R, [H|S]) :- take(T, R, S).
+        perm([], []).
+        perm(L, [H|R]) :- take(L, H, T), perm(T,R).
+        generate_list(1, [1]).
+        generate_list(N, [N|T]) :- N > 1, M is N - 1, generate_list(M, T).
         abs(0, 0).
         abs(N, N) :- N > 0.
         abs(N, M) :- N < 0, M is N * -1.
         n_queens(N, Qs) :- generate_list(N, Qs1), perm(Qs1, Qs), safe_queens(Qs).
-        safe_queens(nil).
-        safe_queens(cons(Q, Qs)) :- safe_queens(Qs, Q, 1), safe_queens(Qs).
-        safe_queens(nil, Y, X).
-        safe_queens(cons(Q, Qs), Q0, D0) :- Q0 =\= Q, Diff is Q0 - Q, abs(Diff, AbsDiff), AbsDiff =\= D0, D1 is D0 + 1, safe_queens(Qs, Q0, D1).
+        safe_queens([]).
+        safe_queens([Q|Qs]) :- safe_queens(Qs, Q, 1), safe_queens(Qs).
+        safe_queens([], Y, X).
+        safe_queens([Q|Qs], Q0, D0) :- Q0 =\= Q, Diff is Q0 - Q, abs(Diff, AbsDiff), AbsDiff =\= D0, D1 is D0 + 1, safe_queens(Qs, Q0, D1).
     "#;
 
     let query = "n_queens(8, Qs).";
