4c7fcc28e91b0a9fb62f458ea30d0a1cd55578a4
William Henderson <william-henderson@outlook.com>
2024-12-03 22:43:05 +0000

    improve WASM bindings

--- core/src/wasm.rs
+++ core/src/wasm.rs
@@ -1,5 +1,3 @@
-use crate::WebPL;
-
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
@@ -8,8 +6,32 @@ pub fn init() {
 }
 
 #[wasm_bindgen]
-pub fn solve(program: &str, query: &str) -> Vec<String> {
-    let mut webpl = WebPL::new(program).unwrap();
-    let solver = webpl.solve(query).unwrap();
-    solver.map(|x| format!("{}={}", x[0].0, x[0].1)).collect()
+pub struct Solver(crate::Solver);
+
+#[wasm_bindgen]
+impl Solver {
+    #[wasm_bindgen(constructor)]
+    pub fn new(program: &str, query: &str) -> Result<Solver, JsValue> {
+        Ok(Solver(
+            crate::Solver::new(program, query).map_err(|e| format!("{:?}", e))?,
+        ))
+    }
+
+    #[wasm_bindgen]
+    pub fn next(&mut self) -> Option<js_sys::Map> {
+        self.0.next().map(solution_to_js)
+    }
+
+    #[wasm_bindgen]
+    pub fn all(&mut self) -> js_sys::Array {
+        self.0.by_ref().map(solution_to_js).collect()
+    }
+}
+
+fn solution_to_js(solution: crate::Solution) -> js_sys::Map {
+    let result = js_sys::Map::new();
+    for (k, v) in solution {
+        result.set(&k.into(), &v.into());
+    }
+    result
 }
