ecce9cc0f32d570ae681fc47713f2ab4ba1d5f4e
William Henderson <william-henderson@outlook.com>
2025-01-16 17:26:01 +0000

    add syntax highlighting with primsjs

--- web/src/components/TextContainer.tsx
+++ web/src/components/TextContainer.tsx
@@ -1,12 +1,19 @@
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/themes/prism.css";
+import "prismjs/components/prism-prolog";
+
 import styles from "./TextContainer.module.css";
 
 export default function TextContainer(props: Readonly<{ placeholder: string, text: string, update: (x: string) => void }>) {
   return (
-    <textarea
-      className={styles.textarea}
+    <Editor
+      value={props.text}
+      onValueChange={code => props.update(code)}
+      highlight={code => highlight(code, languages.prolog)}
+      className={styles.editor}
       placeholder={props.placeholder}
-      content={props.text}
-      onChange={e => props.update(e.target.value)}
+      padding={16}
     />
   );
 }
