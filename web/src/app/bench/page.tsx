068cf9940b395dd5e67b4886dd4b2d8530a7f98f
William Henderson <william-henderson@outlook.com>
2025-03-12 12:01:17 +0000

    support benchmarking with experimental SWIPL

--- web/src/app/bench/page.tsx
+++ web/src/app/bench/page.tsx
@@ -9,25 +9,38 @@ import { run } from "webpl-bench";
 import WebPL from "@/prolog/webpl";
 import WebPLGC from "@/prolog/webpl-gc";
 import SWIPL from "@/prolog/swipl";
+import SWIPLExperimental from "@/prolog/swipl-experimental";
 import TreallaProlog from "@/prolog/trealla-prolog";
 import TauProlog from "@/prolog/tau-prolog";
 
 const WEBPL = new WebPL();
 const WEBPL_GC = new WebPLGC();
 let SWIPL_ = new SWIPL();
+let SWIPL_EXPERIMENTAL = new SWIPLExperimental();
 const TREALLA_PROLOG = new TreallaProlog();
 const TAU_PROLOG = new TauProlog();
+
+const ENGINES = [
+  "WebPL",
+  "WebPL (GC)",
+  "SWI-Prolog",
+  "SWI (Experimental)",
+  "Trealla Prolog",
+  "Tau Prolog"
+];
 
 export default function BenchPage() {
   const [log, setLog] = useState<string>("");
   const [state, setState] = useState<"idle" | "running" | "complete">("idle");
   const [results, setResults] = useState<any>(null);
+  const [enabledEngines, setEnabledEngines] = useState<string[]>(ENGINES);
 
   useEffect(() => {
     (async () => {
       await WEBPL.init();
       await WEBPL_GC.init();
       await SWIPL_.init();
+      await SWIPL_EXPERIMENTAL.init();
       await TREALLA_PROLOG.init();
       await TAU_PROLOG.init();
     })();
@@ -45,6 +58,7 @@ export default function BenchPage() {
     const results: any = {};
 
     for (const engine of [WEBPL, WEBPL_GC]) {
+      if (!enabledEngines.includes(engine.name)) continue;
       results[engine.name] = await run(engine.name, {
         solve: async (program: string, query: string) => {
           await engine.solve(program, query.slice(0, -1) + ", statistics(allocated, Mem).");
@@ -59,23 +73,29 @@ export default function BenchPage() {
       });
     }
 
-    results["SWI-Prolog"] = await run("SWI-Prolog", {
-      solve: async (program: string, query: string) => {
-        await SWIPL_.solve(program, query.slice(0, -1) + ", statistics(stack, Mem).");
-        const result = await SWIPL_.next();
-        return {
-          ok: result !== undefined,
-          memory: result ? parseInt(result.get("Mem")!) : undefined
+    for (const engine of [SWIPL_, SWIPL_EXPERIMENTAL]) {
+      if (!enabledEngines.includes(engine.name)) continue;
+      results[engine.name] = await run(engine.name, {
+        solve: async (program: string, query: string) => {
+          await engine.solve(program, query.slice(0, -1) + ", statistics(stack, Mem).");
+          const result = await engine.next();
+          return {
+            ok: result !== undefined,
+            memory: result ? parseInt(result.get("Mem")!) : undefined
+          }
+        },
+        log: s => setLog(log => log + s),
+        clean: async () => {
+          SWIPL_ = new SWIPL();
+          SWIPL_EXPERIMENTAL = new SWIPLExperimental();
+          await SWIPL_.init();
+          await SWIPL_EXPERIMENTAL.init();
         }
-      },
-      log: s => setLog(log => log + s),
-      clean: async () => {
-        SWIPL_ = new SWIPL();
-        await SWIPL_.init();
-      }
-    });
+      });
+    }
 
     for (const engine of [TREALLA_PROLOG, TAU_PROLOG]) {
+      if (!enabledEngines.includes(engine.name)) continue;
       results[engine.name] = await run(engine.name, {
         solve: async (program: string, query: string) => {
           await engine.solve(program, query);
@@ -109,6 +129,21 @@ export default function BenchPage() {
       {state === "idle" && <span onClick={startBenchmark}>Start Benchmark</span>}
       {state === "running" && <span className={styles.running}>Running...</span>}
       {state === "complete" && <span onClick={getResults}>Download Results</span>}
+
+      <div>
+        {ENGINES.map(engine => (
+          <label key={engine}>
+            <input
+              type="checkbox"
+              checked={enabledEngines.includes(engine)}
+              onChange={e => setEnabledEngines(enabledEngines => {
+                if (e.target.checked) return [...enabledEngines, engine];
+                else return enabledEngines.filter(e => e !== engine);
+              })} />
+            {engine}
+          </label>
+        ))}
+      </div>
 
       <textarea
         value={log}
